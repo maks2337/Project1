df7970548511a92ed40e691abb4e7065cddcafd1
maks2337 <maksimzinovev13031@gmail.com>
2025-05-15 12:18:34 +0300

    Добавьте файлы проекта.

---
+++ Project1/FileName.cpp
@@ -0,0 +1,17 @@
+﻿//ПРАКТИЧЕСКАЯ РАБОТА № 1. Потоковый ввод/вывод в C++. Написать программу пересчета веса из фунтов в килограммы
+#include<iostream>
+using namespace std;
+
+const float funt = 405.9;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	float kg = 0, vfunt = 0;
+	cout << "Пересчет веса из фунта в килограммы" << endl;
+	cout << "Введите вес в фунтах ->";
+	cin >> vfunt;
+	kg = (vfunt * funt) / 1000;
+	cout << vfunt << " фунт(а/ов) - это " << kg << " килограмм" << endl;
+	system("pause");
+	return 0;
+	}
